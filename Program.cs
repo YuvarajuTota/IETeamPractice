51f3fdad3e31e504327a48bc8d3944878b618eb3
YuvarajuTota <yuvaraju.tota@amzur.com>
2023-03-03 17:28:25 +0530

    name change

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 namespace CSharp
 {
+
+    class Empoloyee
+    {
+        public string FirstName;
+        public string LastName;
+        public float salary;
+
+        public void PrintFullName()
+        {
+            System.Console.WriteLine(FirstName + " " + LastName);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
             System.Console.WriteLine("This is the test file");
             System.Console.WriteLine("Hi Team, I have created this repository to work on.");
+            System.Console.WriteLine("One more statement");
+
+            Empoloyee E = new Empoloyee();
+            E.FirstName = "IE";
+            E.LastName = "Team";
+            E.PrintFullName();
         }
     }
 }
