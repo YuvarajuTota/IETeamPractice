ef67fead2ccc7eed138d444b74cb8349b2547255
YuvarajuTota <yuvaraju.tota@amzur.com>
2023-03-03 17:21:11 +0530

    init

--- Program.cs
+++ Program.cs
@@ -6,6 +6,7 @@ namespace CSharp
         static void Main(string[] args)
         {
             System.Console.WriteLine("This is the test file");
+            System.Console.WriteLine("Hi Team, I have created this repository to work on.");
         }
     }
 }
